e85efcbfa141cf404da95e3993e7a9cc96681388
Jinisha Patel <jinishakheni@gmail.com>
2024-04-30 22:07:37 +0200

    Implement forgot password page

--- src/main.jsx
+++ src/main.jsx
@@ -6,13 +6,18 @@ import App from "./App.jsx";
 // Import styles of packages that you've installed.
 // All packages except `@mantine/hooks` require styles imports
 import "@mantine/core/styles.css";
+import "@mantine/notifications/styles.css";
+
 import "./styles/global.css";
 import theme from "./styles/theme.js";
+
 import { BrowserRouter } from "react-router-dom";
+import { Notifications } from "@mantine/notifications";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <MantineProvider theme={theme} defaultColorScheme="light">
+      <Notifications position="top-right" autoClose={4000} zIndex={1000} />
       <BrowserRouter>
         <App />
       </BrowserRouter>
