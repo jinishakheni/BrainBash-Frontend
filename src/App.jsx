3149bff6606b2545fc4082dbfbdba8106fc51348
KemalGokten <ukemalgokten@gmail.com>
2024-05-01 12:38:27 +0200

    Add Reset password route and 404 page not found route

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,15 @@
 import { Route, Routes } from "react-router-dom";
 import HomePage from "./pages/HomePage";
-import ForgotPasswordPage from "./pages/ForgotPasswordPage";
+import ResetPassword from "./pages/passwordPages/ResetPassword";
+import ForgotPasswordPage from "./pages/passwordPages/ForgotPasswordPage";
 
 function App() {
   return (
     <Routes>
       <Route path="/" element={<HomePage />} />
       <Route path="/forgot-password" element={<ForgotPasswordPage />} />
+      <Route path="/reset-password/:token" element={<ResetPassword />} />
+      <Route path="*" element={<h1>404 Page Not Found</h1>} />
     </Routes>
   );
 }
