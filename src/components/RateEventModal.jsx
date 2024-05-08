f42ed622304806097992b0663a392fdf1a534ed8
Jinisha Patel <jinishakheni@gmail.com>
2024-05-08 13:38:40 +0200

    Event rating changes

---
+++ src/components/RateEventModal.jsx
@@ -0,0 +1,33 @@
+import { Button, Rating } from "@mantine/core";
+import { useContext, useState } from "react";
+import { AuthContext } from "../contexts/AuthContext";
+
+const RateEventModal = ({ event, updateEvent }) => {
+  const [rating, setRating] = useState(0);
+  const { user } = useContext(AuthContext);
+  const handleSubmit = () => {
+    console.log({ event, rating });
+    const ratingBy = event.ratingBy ? event.ratingBy.length : 0;
+    const payload = {
+      rating: event.rating
+        ? (event.rating * ratingBy + rating) / (ratingBy + 1)
+        : rating,
+      ratingBy: [...event.ratingBy, user.userId],
+    };
+    updateEvent(event._id, payload);
+  };
+  return (
+    <>
+      <Rating
+        value={rating}
+        fractions={2}
+        size="xl"
+        onChange={setRating}
+        mb="md"
+      />
+      <Button onClick={handleSubmit}>Submit</Button>
+    </>
+  );
+};
+
+export default RateEventModal;
